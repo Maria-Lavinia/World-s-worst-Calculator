00aa6a5b1cc5882ac41563a2ed4330fb2153eebc
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-08-29 15:53:21 +0200

    making it work

--- script.js
+++ script.js
@@ -1,88 +1,89 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", settingUp);
 
-  let firstNumber = document.querySelector("#firstnumber").value;
+let firstNumber = document.querySelector("#firstnumber").value;
 let secondNumber = document.querySelector("#secondnumber").value;
 let operatordropDown = document.querySelector("#operator").value;
 let decimalsDropDown = document.querySelector("#decimals").value;
 let checkBox = document.querySelector("#doround").checked;
 let result;
- 
 
-function settingUp(){
-    console.log("settingUp");
+function settingUp() {
+  console.log("settingUp");
 
-    document.querySelector("#firstnumber").addEventListener("input", readNumbers);
-    document.querySelector("#secondnumber").addEventListener("input", readNumbers);
-    document.querySelector("#operator").addEventListener("change", readOperator);
-    document.querySelector("#calculate").addEventListener("click", calculation);
-    document.querySelector("#decimals").addEventListener("change", readDecimals);
-    document.querySelector("#doround").addEventListener("change", readCheckBox);
-    document.querySelector("#clear").addEventListener("click", clearResult);
+  document.querySelector("#firstnumber").addEventListener("input", readNumbers);
+  document.querySelector("#secondnumber").addEventListener("input", readNumbers);
+  document.querySelector("#operator").addEventListener("change", readOperator);
+  document.querySelector("#calculate").addEventListener("click", calculate);
+  document.querySelector("#decimals").addEventListener("change", readDecimals);
+  document.querySelector("#doround").addEventListener("change", readCheckBox);
+  document.querySelector("#clear").addEventListener("click", clearResult);
 }
 
-function readNumbers(){
+function readNumbers() {
   console.log("readNumbers");
 
   firstNumber = Number(document.querySelector("#firstnumber").value);
   secondNumber = Number(document.querySelector("#secondnumber").value);
-  
+
   console.log(firstNumber);
   console.log(secondNumber);
-  
 }
 
-function readOperator(){
-    console.log("readOperator");
-    operatordropDown = document.querySelector("#operator").value;
-    console.log(operatordropDown);
+function readOperator() {
+  console.log("readOperator");
+  operatordropDown = document.querySelector("#operator").value;
+  console.log(operatordropDown);
 }
 
-function readCheckBox(){
-    checkBox = document.querySelector("#doround").checked;
-    console.log(checkBox);
+function readCheckBox() {
+  checkBox = document.querySelector("#doround").checked;
+  console.log(checkBox);
 }
 
-function readDecimals(){
-    console.log("readDecimals");
+function readDecimals() {
+  console.log("readDecimals");
 
-    decimalsDropDown = document.getElementById("#decimals").value;
-    console.log(decimalsDropDown);
+  decimalsDropDown = document.querySelector("#decimals").value;
+  console.log(decimalsDropDown);
 }
 
-function calculation(){
-    console.log("calculation");
-    if (checkBox === true) {
-      result = result.toFixed(decimalsDropDown);
+function calculate() {
+  console.log("calculate");
+
+  if (operatordropDown === "add") {
+    result = Number(document.querySelector("#firstnumber").value) + Number(document.querySelector("#secondnumber").value);
+  } else if (operatordropDown === "sub") {
+    result = firstNumber - secondNumber;
+  } else if (operatordropDown === "mul") {
+    result = firstNumber * secondNumber;
+  } else if (operatordropDown === "div") {
+    result = firstNumber / secondNumber;
   }
-
-    else if (operatordropDown == "add") {
-        result = firstNumber + secondNumber;
-    } else if (operatordropDown === "sub") {
-        result = firstNumber - secondNumber;
-    } else if (operatordropDown === "mul") {
-        result = firstNumber * secondNumber;
-    } else if (operatordropDown === "div") {
-        result = firstNumber / secondNumber;
-    }
-
-    showResult();
-    
+  if (checkBox === true) {
+    result = result.toFixed(decimalsDropDown);
+  }
+  if (checkBox === false) {
+    result = result;
+  }
+  showResult();
+  readNumbers();
 }
 
-function showResult(){
-    console.log(result);
-    let li = document.createElement("li");
-    const resultNote = document.createTextNode(result.toString());
+function showResult() {
+  console.log(result);
+  document.querySelector("#firstnumber").value = result;
+  let li = document.createElement("li");
+  const resultNote = document.createTextNode(result.toString());
 
-    li.appendChild(resultNote);
-    
-    document.querySelector("#results").appendChild(li);
-    document.querySelector("#results").scrollTo(0, 10000000);
+  li.appendChild(resultNote);
+
+  document.querySelector("#results").appendChild(li);
+  document.querySelector("#results").scrollTo(0, 10);
 }
 
-function clearResult(){
-    console.log("clearResult");
+function clearResult() {
+  console.log("clearResult");
 
-    document.querySelector("#results").innerHTML = "";
+  document.querySelector("#results").innerHTML = "";
 }
